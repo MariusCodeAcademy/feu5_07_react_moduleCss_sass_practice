f15c7aedbabefd2810382ce9049734e502037ba6
Marius CA <mariuskcodeacademy@gmail.com>
2023-03-28 10:57:01 +0300

    counter center

--- src/components/sections/countDown/SingleCountDown.jsx
+++ src/components/sections/countDown/SingleCountDown.jsx
@@ -2,7 +2,12 @@ import React from 'react';
 import css from './SingleCountDown.module.css';
 
 function SingleCountDown() {
-  return <div>SingleCountDown</div>;
+  return (
+    <div className={css.circle}>
+      <p className={css.value}>0</p>
+      <h3 className={css.time}>Days</h3>
+    </div>
+  );
 }
 
 export default SingleCountDown;
