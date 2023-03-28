9983bc5d8c414cf8e4f317b1b01848ae31d6e9f9
Marius CA <mariuskcodeacademy@gmail.com>
2023-03-28 10:45:45 +0300

    title

--- src/components/sections/countDown/CountDownSecion.jsx
+++ src/components/sections/countDown/CountDownSecion.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import Grid from '../../ui/grid/Grid';
 import css from './CountDownSecion.module.css';
+import SingleCountDown from './SingleCountDown';
 
 function CountDownSecion() {
   return (
     <section className={`container ${css.section}`}>
-      <h2>Title</h2>
+      <h2 className={css.title}>
+        Count Every Second Until the <span>Event</span>
+      </h2>
       <Grid cols={4}>
-        <h2>item</h2>
-        <h2>item</h2>
-        <h2>item</h2>
-        <h2>item</h2>
+        <SingleCountDown />
+        <SingleCountDown />
+        <SingleCountDown />
+        <SingleCountDown />
       </Grid>
     </section>
   );
