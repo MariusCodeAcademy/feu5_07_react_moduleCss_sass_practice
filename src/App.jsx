b2d126d31b9a93e27d7ddf2f9651a746eb200868
Marius CA <mariuskcodeacademy@gmail.com>
2023-03-28 09:36:26 +0300

    problem

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ function App() {
     <div className='container'>
       <h1>New project</h1>
       <Button>Click me now!!!</Button>
+      {/* sitas button gauna tik Button skirta css */}
       <button className='btn'>Subscirbe</button>
     </div>
   );
