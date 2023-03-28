1b04b9bffc404d2be04e84fbc27efa807fe2ec88
Marius CA <mariuskcodeacademy@gmail.com>
2023-03-28 11:33:21 +0300

    switch way

--- src/components/sections/countDown/CountDownSecion.jsx
+++ src/components/sections/countDown/CountDownSecion.jsx
@@ -10,10 +10,10 @@ function CountDownSecion() {
         Count Every Second Until the <span>Event</span>
       </h2>
       <Grid cols={4}>
-        <SingleCountDown time='Days' value={5} circle={'violet'} color='tomato' />
-        <SingleCountDown />
-        <SingleCountDown />
-        <SingleCountDown />
+        <SingleCountDown time='Days' value={5} circle={'violet'} />
+        <SingleCountDown time='Hours' value={5} circle={'sky'} />
+        <SingleCountDown time='Minutes' value={5} circle={'blue'} />
+        <SingleCountDown time='Seconds' value={5} circle={'green'} />
       </Grid>
     </section>
   );
