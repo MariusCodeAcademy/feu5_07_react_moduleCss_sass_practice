4536f2619b349f913afc7f46f0a7bd0b798a7a81
Marius CA <mariuskcodeacademy@gmail.com>
2023-03-28 09:35:41 +0300

    the problem

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './styles/reset.css';
 import './styles/App.css';
+import Button from './components/ui/button/Button';
 
 function App() {
   return (
     <div className='container'>
       <h1>New project</h1>
+      <Button>Click me now!!!</Button>
+      <button className='btn'>Subscirbe</button>
     </div>
   );
 }
