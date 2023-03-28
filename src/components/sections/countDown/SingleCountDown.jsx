1f69756a6d1bc92d0294181d37abae183e20d4ec
Marius CA <mariuskcodeacademy@gmail.com>
2023-03-28 11:36:57 +0300

    color classes

--- src/components/sections/countDown/SingleCountDown.jsx
+++ src/components/sections/countDown/SingleCountDown.jsx
@@ -5,26 +5,26 @@ function SingleCountDown(props) {
   // i value paragrafa paduoti reikse is props
   // value ir h3 elementams uzdeti klase pagal props
   // circle elementui uzdeti klase pagal props
-  let colorClass = '';
-  switch (props.circle) {
-    case 'violet':
-      colorClass = css.violet;
-      break;
-    case 'sky':
-      colorClass = css.sky;
-      break;
-    case 'blue':
-      colorClass = css.blue;
-      break;
-    case 'green':
-      colorClass = css.green;
-      break;
-    default:
-      colorClass = '';
-  }
+  // let colorClass = '';
+  // switch (props.circle) {
+  //   case 'violet':
+  //     colorClass = css.violet;
+  //     break;
+  //   case 'sky':
+  //     colorClass = css.sky;
+  //     break;
+  //   case 'blue':
+  //     colorClass = css.blue;
+  //     break;
+  //   case 'green':
+  //     colorClass = css.green;
+  //     break;
+  //   default:
+  //     colorClass = '';
+  // }
 
   return (
-    <div className={`${css.circle} ${colorClass}`}>
+    <div className={`${css.circle} ${css[props.circle]}`}>
       <p className={css.value}>{props.value}</p>
       <h3 className={css.time}>{props.time}</h3>
     </div>
