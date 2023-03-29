53d5efdd50a2647f8281ec29267e72bb05ef4702
Marius CA <mariuskcodeacademy@gmail.com>
2023-03-29 11:22:27 +0300

    raise state up

--- src/components/sections/conference/filterItem/FilterItem.jsx
+++ src/components/sections/conference/filterItem/FilterItem.jsx
@@ -1,20 +1,17 @@
 /* eslint-disable jsx-a11y/interactive-supports-focus */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
-import React, { useState } from 'react';
+import React from 'react';
 import css from './FilterItem.module.css';
 
 // 1. istrinti state
 // 2, mygtuko paspaudimas turi iskviesti gauta is props fn
 // 3. activeClass priklausys nuo props
 
-function FilterItem({ title, date }) {
-  const [isActive, setIsActive] = useState(false);
+function FilterItem({ title, date, isActive, onToggle }) {
   const activeClass = isActive ? css.active : '';
   return (
     <div
-      onClick={() => {
-        setIsActive(!isActive);
-      }}
+      onClick={onToggle}
       className={[css.redPill, activeClass].join(' ')}
       role="button"
     >
