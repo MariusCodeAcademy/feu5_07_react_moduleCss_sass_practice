0c32b39099467c91000143acb1895b2e5adef663
Marius CA <mariuskcodeacademy@gmail.com>
2023-03-29 10:02:25 +0300

    Filter start

--- src/components/sections/conference/ConferenceSection.jsx
+++ src/components/sections/conference/ConferenceSection.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import SectionTitle from '../../ui/title/SectionTitle';
 import css from './ConferenceSection.module.css';
+import FilterItem from './filterItem/FilterItem';
 
 function ConferenceSection() {
   return (
     <section className={css.section}>
       <SectionTitle title="Schedule" subtitle="Conference Schedule" />
+      <div className="control">
+        <FilterItem title="Day 1" date="2020-02-02" />
+      </div>
     </section>
   );
 }
---
+++ src/components/sections/conference/filterItem/FilterItem.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import css from './FilterItem.module.css';
+
+function FilterItem() {
+  return (
+    <div className={css.redPill}>
+      <h3 className={css.title}>title</h3>
+      <p>date</p>
+    </div>
+  );
+}
+
+export default FilterItem;
