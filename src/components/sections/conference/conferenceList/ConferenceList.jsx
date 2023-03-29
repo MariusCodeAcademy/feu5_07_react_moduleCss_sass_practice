63f491cbc443271bbcee5e4b0168bbffbdec30d3
Marius CA <mariuskcodeacademy@gmail.com>
2023-03-29 11:25:12 +0300

    ConferenceList start

---
+++ src/components/sections/conference/conferenceList/ConferenceList.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import css from './ConferenceList.module.css';
+
+function ConferenceList() {
+  return (
+    <div className={css.wrap}>
+      <div className="info">info</div>
+      <div className="about">about</div>
+    </div>
+  );
+}
+
+export default ConferenceList;
