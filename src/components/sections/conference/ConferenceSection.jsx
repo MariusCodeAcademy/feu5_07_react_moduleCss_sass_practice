4687565ce8cf8f3b09280bba58e558d0cb474948
Marius CA <mariuskcodeacademy@gmail.com>
2023-03-29 11:22:39 +0300

    handleToggleFilter

--- src/components/sections/conference/ConferenceSection.jsx
+++ src/components/sections/conference/ConferenceSection.jsx
@@ -4,17 +4,45 @@ import SectionTitle from '../../ui/title/SectionTitle';
 import css from './ConferenceSection.module.css';
 import FilterItem from './filterItem/FilterItem';
 
+const initFilters = [
+  {
+    id: 1,
+    title: 'Day 1',
+    date: '2020-02-02',
+    isActive: true,
+  },
+  {
+    id: 2,
+    title: 'Day 2',
+    date: '2020-02-05',
+    isActive: false,
+  },
+  {
+    id: 3,
+    title: 'Day 3',
+    date: '2020-02-06',
+    isActive: false,
+  },
+];
+
 function ConferenceSection() {
-  const [filters, setFilters] = useState([
-    { id: 1, title: 'Day 1', date: '2020-02-02', isActive: true },
-    { id: 2, title: 'Day 2', date: '2020-02-05', isActive: false },
-    { id: 3, title: 'Day 3', date: '2020-02-06', isActive: false },
-  ]);
+  const [filters, setFilters] = useState(initFilters);
+  // console.log('initFilters ===', initFilters);
 
   function handleToggleFilter(idOfToggleEl) {
-    console.log('handleToggleFilter ===', idOfToggleEl);
+    // console.log('handleToggleFilter ===', idOfToggleEl);
     // pakeisti to el ant kurio paspausta isActive i priesinga
-    // visu kitu i false
+    setFilters((prevFilter) =>
+      prevFilter.map((fObj) => {
+        // el ant kurio paspausta
+        if (fObj.id === idOfToggleEl) {
+          // isActive i priesinga arba true
+          return { ...fObj, isActive: true };
+        }
+        // visu kitu i false
+        return { ...fObj, isActive: false };
+      }),
+    );
   }
   return (
     <section className={css.section}>
