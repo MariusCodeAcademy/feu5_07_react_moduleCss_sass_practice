f642cc2948e4b2bf0f1f4839acab760a1821ea29
Marius CA <mariuskcodeacademy@gmail.com>
2023-03-29 09:37:47 +0300

    font awesome

--- readme.md
+++ readme.md
@@ -2,4 +2,7 @@
 
 url - https://droitthemes.com/html/saasland/home-event.html
 
-1.
+## instal font awesome
+
+1. `npm i font-awesome@^4` - suinstaliuosim font awesome versija
+2. `import 'font-awesome/css/font-awesome.min.css';` itrauksim fa css i main.jsx
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import 'font-awesome/css/font-awesome.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
