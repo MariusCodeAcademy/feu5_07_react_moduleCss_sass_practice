edcb09780af68782e1c3e071d8fc22b86a07a647
Marius CA <mariuskcodeacademy@gmail.com>
2023-03-29 12:11:32 +0300

    citem works

--- src/components/sections/conference/conferenceList/CItem.jsx
+++ src/components/sections/conference/conferenceList/CItem.jsx
@@ -1,20 +1,21 @@
 import css from './CItem.module.css';
 // surasyti visas reiksmes is gautu props
-function CItem() {
+function CItem({ item }) {
+  const { title, room, speaker, img, date } = item;
   return (
     <div className={css.flex}>
       <div className={css.info}>
-        <img src="" alt="img" />
+        <img className={css.img} src={img} height="60" alt="img" />
         <p className={css.dataRow}>
-          icon <span>date</span>
+          icon <span>{date}</span>
         </p>
         <p className={css.dataRow}>
-          icon <span>room</span>
+          icon <span>{room}</span>
         </p>
       </div>
       <div className="about">
-        <h3>title</h3>
-        <p>by speaker</p>
+        <h3>{title}</h3>
+        <p>by {speaker}</p>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
           commodi illum ipsa, explicabo distinctio veniam.
